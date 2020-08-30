dd28c27bd08f772bbcd2b01107f8c7b87e99d0ae
Daniel Gallegos Alia <dani@theyellowfactory.com>
2020-08-30 17:48:28 +0200

    Canvis de nom

--- lib/navigation/listcreateupdate_flow.dart
+++ lib/navigation/listcreateupdate_flow.dart
@@ -1,42 +1,31 @@
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:yellow_naples/navigation/navigation.dart';
-import 'package:yellow_naples/view_models/common.dart';
 import 'package:yellow_naples/view_models/view_model.dart';
 
-//TODO: Per poder abstreure el comportament de listcreateupdate, caldrà fer les següents modificacions:
-//1.-Fer que el constructor de ViewModel no tingui com a paràmetre el context, sinó que sigui un paràmetre de initialize (això farà de retruc que Navigation també pugui implementar OnTimeInitialize que haurà de tenir un paràmetre)
-//2.-
-
 //Possible transition states
-// enum ListCreateUpdateTS { List, Create, Update }
+enum ListCreateUpdateFlow { List, Create, Update }
 
-// class ListCreateUpdateNavigationFlow extends NavigationFlow<ListCreateUpdateTS> {
-//   final CreateViewModelFunction _createCreateViewModel;
-//   final CreateViewModelFunction _createUpdateViewModel;
+class ListCreateUpdateStandardNavigationFlow extends NavigationFlow<ListCreateUpdateFlow> {
+  final CreateViewModelFunction _createCreateViewModel;
+  final CreateViewModelFunction _createUpdateViewModel;
 
-//   ListCreateUpdateNavigationFlow(CreateViewModelFunction createListViewModel,
-//       this._createCreateViewModel, this._createUpdateViewModel)
-//       : super(ListCreateUpdateTS.List, createListViewModel) {
-//     addTransition(ListCreateUpdateTS.List, ListCreateUpdateTS.Create, _createCreateViewModel);
-//     addTransition(ListCreateUpdateTS.List, ListCreateUpdateTS.Update, _createUpdateViewModel);
-//   }
-// }
+  ListCreateUpdateStandardNavigationFlow(CreateViewModelFunction createListViewModel,
+      this._createCreateViewModel, this._createUpdateViewModel)
+      : super(ListCreateUpdateFlow.List, createListViewModel) {
+    addTransition(ListCreateUpdateFlow.List, ListCreateUpdateFlow.Create, _createCreateViewModel);
+    addTransition(ListCreateUpdateFlow.List, ListCreateUpdateFlow.Update, _createUpdateViewModel);
+  }
+}
 
-// class ListCreateUpdateNavigation extends ListCreateUpdateNavigationFlow {
-//   ListCreateUpdateNavigation()
-//       : super((context) => ListModelViewModel(context), (context) => CreateModelRequestVM(context),
-//             (context) => UpdateModelRequestVM(context));
-// }
+class ListCreateUpdateStandardNavigationModel extends NavigationModel<ListCreateUpdateFlow> {
+  ListCreateUpdateStandardNavigationModel(ListCreateUpdateStandardNavigationFlow navigationFlow)
+      : super(navigationFlow);
 
-// class ListCreateUpdateNavigationModel extends NavigationModel<ListCreateUpdateTS> {
-//   ListCreateUpdateNavigationModel() : super(ListCreateUpdateNavigation());
-
-//   @override
-//   Widget get widget {
-//     return MultiProvider(providers: [
-//       //The Navigation flow in this case needs a param to navigate from list to edit
-//       ChangeNotifierProvider<UidParam>(create: (_) => UidParam(null)),
-//     ], child: NavigationModel<ListCreateUpdateTS>(ListCreateUpdateNavigation()).widget);
-//   }
-// }
+  @override
+  Widget get widget {
+    return
+        //The Navigation flow in this case needs a param to navigate from list to edit
+        ChangeNotifierProvider<UidParam>(create: (_) => UidParam(null), child: super.widget);
+  }
+}
