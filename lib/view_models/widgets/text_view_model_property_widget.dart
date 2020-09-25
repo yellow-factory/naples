327d5192169b82289482a08cd274b7574769595b
Daniel Gallegos Alia <dani@theyellowfactory.com>
2020-09-25 09:57:30 +0200

    Reorganize

---
+++ lib/view_models/widgets/text_view_model_property_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:yellow_naples/view_models/properties/properties.dart';
+import 'package:provider/provider.dart';
+
+class TextViewModelPropertyWidget extends StatelessWidget {
+  final TextInputType textInputType;
+  final List<TextInputFormatter> textInputFormatters;
+
+  TextViewModelPropertyWidget({this.textInputType, this.textInputFormatters});
+
+  @override
+  Widget build(BuildContext context) {
+    final property = context.watch<TextViewModelProperty>();
+    return TextFormField(
+      key: UniqueKey(),
+      initialValue: property.currentValue,
+      decoration: InputDecoration(
+        //filled: true,
+        hintText: property.hint != null ? property.hint() : null,
+        labelText: property.label(),
+      ),
+      enabled: property.editable,
+      autofocus: property.autofocus,
+      validator: (_) => property.validate(),
+      keyboardType: textInputType,
+      inputFormatters: textInputFormatters,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      onChanged: (value) {
+        property.currentValue = value;
+        property.update();
+        //TODO: Quan es fa l'update es passa el valor al model, i no sé si en tots
+        //els casos és el que volem, potser podríem tenir una opció (autoupdate)
+        //si no ho fem, els visibles, editables, etc. no s'actualitzaran correctament,
+        //però no sé quan és el millor moment, si quan es fa el canvi o quan es fa l'update explícit de la viewmodel
+      },
+    );
+  }
+}
