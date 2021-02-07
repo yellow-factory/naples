706ebb187ca45332be94c4701c479da6d5c9cecb
Daniel Gallegos Alia <dani@theyellowfactory.com>
2021-02-07 19:25:24 +0100

    Better filter list

--- lib/src/list/filter_list.dart
+++ lib/src/list/filter_list.dart
@@ -2,50 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:navy/navy.dart';
 
-//TODO: Es podria generalitzar i que enlloc de ser per String fos per U
-
-//T tipus de dades de la llista
+/// Filter list of items, defining the value to filter with getItemValue
+/// and the filter value with filterBy.
+/// The value to filter is always a String filtered with the contains clause.
+/// T Data type of the List to filter
 class FilterList<T> extends StatelessWidget {
   final List<T> items;
-  final FunctionOf1<T, String> filter;
+  final FunctionOf1<T, String> getItemValue;
   final String filterBy;
   final FunctionOf1<List<T>, Widget> builder;
 
   FilterList({
     @required this.items,
     @required this.builder,
-    @required this.filter,
+    @required this.getItemValue,
     @required this.filterBy,
     Key key,
   }) : super(key: key);
 
-  // bool Function(T) get _filterPredicate {
-  //   try {
-  //     if (filterBy == null) return (x) => true;
-  //     var filterByX = filterBy.toLowerCase().trim();
-  //     if (filterByX.isEmpty) return (x) => true;
-  //     return (x) => filter(x).toLowerCase().contains(filterByX);
-  //   } on Exception catch (e) {
-  //     throw e;
-  //   }
-  // }
-
   List<T> _filterItems(List<T> items) {
-      if (filter == null) return items;
-      if (filterBy == null) return items;
-      var filterByX = filterBy.toLowerCase().trim();
-      if (filterByX.isEmpty) return items;
-      var predicate = (x) {
-        if (x == null) return false;
-        var f = filter(x) ?? "";
-        return f.toLowerCase().trim().contains(filterByX);
-      };
-      return items.where(predicate).toList();
+    if (getItemValue == null) return items;
+    if (filterBy == null) return items;
+
+    var curatedFilterBy = filterBy.toLowerCase().trim();
+    if (curatedFilterBy.isEmpty) return items;
+
+    var predicate = (item) {
+      if (item == null) return false;
+      var itemValue = getItemValue(item);
+      if (itemValue == null) return false;
+      return itemValue.toLowerCase().trim().contains(curatedFilterBy);
+    };
+
+    return items.where(predicate).toList();
   }
 
   @override
   Widget build(BuildContext context) {
-      var fitems = _filterItems(items);
-      return builder(fitems);
+    var fitems = _filterItems(items);
+    return builder(fitems);
   }
 }
