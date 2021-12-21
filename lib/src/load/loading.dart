22a80c7e0c13c0da2c12b412dc565e21de013869
Daniel Gallegos Alia <dani@theyellowfactory.com>
2021-12-21 19:05:35 +0100

    Easier loading widget

--- lib/src/load/loading.dart
+++ lib/src/load/loading.dart
@@ -1,42 +1,36 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:navy/navy.dart';
-import 'package:provider/single_child_widget.dart';
 
-class Loading extends SingleChildStatefulWidget {
-  const Loading({Key? key, required Widget? child}) : super(key: key, child: child);
+class Loading extends StatefulWidget {
+  final Widget child;
+  const Loading({Key? key, required this.child}) : super(key: key);
 
   @override
   State<Loading> createState() => _LoadingState();
 }
 
-class _LoadingState extends SingleChildState<Loading> {
+class _LoadingState extends State<Loading> {
   bool _loading = false;
 
   @override
-  Widget buildWithChild(BuildContext context, Widget? child) {
-    return ifNotNullFunctionOf1(
-      child,
-      (Widget c) {
-        return NotificationListener<LoadingNotification>(
-            onNotification: (notification) {
-              scheduleMicrotask(() {
-                setState(() {
-                  _loading = notification.loading;
-                });
-              });
-              return true;
-            },
-            child: Column(
-              children: [
-                if (_loading) LinearProgressIndicator(),
-                Expanded(child: c),
-              ],
-            ));
+  Widget build(BuildContext context) {
+    return NotificationListener<LoadingNotification>(
+      onNotification: (notification) {
+        if (_loading == notification.loading) return true;
+        scheduleMicrotask(() {
+          setState(() {
+            _loading = notification.loading;
+          });
+        });
+        return true;
       },
-      SizedBox(),
+      child: Column(
+        children: [
+          if (_loading) LinearProgressIndicator(),
+          Expanded(child: widget.child),
+        ],
+      ),
     );
   }
 }
