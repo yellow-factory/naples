b6def15f19eb0430a465a2febcd8a24f28a85064
Daniel Gallegos Alia <dani@theyellowfactory.com>
2022-09-07 09:27:44 +0200

    Propietats de tipus col·lecció

---
+++ lib/src/edit/properties/collection_field.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:navy/navy.dart';
+import '../collection_editor.dart';
+
+class CollectionField<T> extends StatefulWidget {
+  final String name;
+  final FunctionOf0<Future<String>>? description;
+  final double width;
+  final Iterable<T>? initialValue;
+  final FunctionOf1<T, String> itemTitle;
+  final FunctionOf0<T> createT;
+  final FunctionOf1<T, T> updateT;
+  final FunctionOf1<T, Widget>? createWidget;
+  final FunctionOf1<T, Widget> updateWidget;
+  final ActionOf1<List<T>>? onChanged;
+
+  const CollectionField(
+      {Key? key,
+      required this.name,
+      this.description,
+      this.width = 800,
+      required this.itemTitle,
+      this.initialValue,
+      required this.createT,
+      required this.updateT,
+      required this.createWidget,
+      required this.updateWidget,
+      this.onChanged})
+      : super(key: key);
+
+  @override
+  State<CollectionField> createState() => _CollectionFieldState<T>();
+}
+
+class _CollectionFieldState<T> extends State<CollectionField<T>> {
+  late final List<T> _items;
+  T? itemToEdit;
+  T? itemEdited;
+  final _formKey = GlobalKey<FormState>();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _items = widget.initialValue != null ? List<T>.from(widget.initialValue!) : <T>[];
+    _updateDescription();
+  }
+
+  Future<void> _updateDescription() async {
+    if (widget.description != null) {
+      _descriptionController.text = await widget.description!();
+      return;
+    }
+    _descriptionController.text = "Collection of ${typeOf<T>()}(${_items.length})";
+  }
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _showSelectDialog() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: SizedBox(
+            height: 500,
+            child: CollectionEditor<T>(
+              title: () => "Collection of ${widget.name}",
+              items: _items,
+              itemTitle: widget.itemTitle,
+              updateWidget: (itemToUpdate) {
+                itemToEdit = itemToUpdate;
+                itemEdited = widget.updateT(itemToUpdate);
+                return Form(
+                  key: _formKey,
+                  child: widget.updateWidget(itemEdited as T),
+                );
+              },
+              update: () {
+                if (itemEdited == null) {
+                  throw Exception('The createItem cannot be null in this context');
+                }
+                if (_formKey.currentState == null) {
+                  throw Exception('The form containing the createWidget does not exist');
+                }
+                var formState = _formKey.currentState!;
+                if (!formState.validate()) return;
+                formState.save();
+                _items.remove(itemToEdit);
+                _items.add(itemEdited as T);
+                if (widget.onChanged != null) widget.onChanged!(_items);
+              },
+              createWidget: () {
+                itemEdited = widget.createT();
+                if (widget.createWidget == null) return const Placeholder();
+                return Form(
+                  key: _formKey,
+                  child: widget.createWidget!(itemEdited as T),
+                );
+              },
+              create: () async {
+                if (itemEdited == null) {
+                  throw Exception('The createItem cannot be null in this context');
+                }
+                if (_formKey.currentState == null) {
+                  throw Exception('The form containing the createWidget does not exist');
+                }
+                var formState = _formKey.currentState!;
+                if (!formState.validate()) return;
+                formState.save();
+
+                _items.add(itemEdited as T);
+                await _updateDescription();
+                if (widget.onChanged != null) widget.onChanged!(_items);
+              },
+              delete: (itemToDelete) {
+                _items.remove(itemToDelete);
+                if (widget.onChanged != null) widget.onChanged!(_items);
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _descriptionController,
+      readOnly: true,
+      enabled: true,
+      autofocus: false,
+      decoration: InputDecoration(
+        labelText: widget.name,
+        suffixIcon: IconButton(
+          onPressed: _showSelectDialog,
+          icon: const Icon(Icons.edit),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/edit/properties/collection_form_field.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:naples/src/edit/properties/collection_field.dart';
+import 'package:navy/navy.dart';
+
+class CollectionFormField<T> extends FormField<Iterable<T>> {
+  CollectionFormField({
+    Key? key,
+    required String name,
+    FunctionOf0<Future<String>>? description,
+    double width = 800,
+    required FunctionOf1<T, String> itemTitle,
+    Iterable<T>? initialValue,
+    required FunctionOf0<T> createT,
+    required FunctionOf1<T, T> updateT,
+    required FunctionOf1<T, Widget>? createWidget,
+    required FunctionOf1<T, Widget> updateWidget,
+    FormFieldSetter<Iterable<T>>? onSaved,
+  }) : super(
+          key: key,
+          onSaved: onSaved,
+          initialValue: initialValue,
+          builder: (FormFieldState<Iterable<T>> state) {
+            return CollectionField<T>(
+              createT: createT,
+              createWidget: createWidget,
+              itemTitle: itemTitle,
+              name: name,
+              initialValue: initialValue,
+              updateT: updateT,
+              updateWidget: updateWidget,
+              description: description,
+              width: width,
+              onChanged: (t) => state.didChange(t),
+            );
+          },
+        );
+}
