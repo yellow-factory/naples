c15852358e2c0e4b12a2bd4a726fc3c19eb18228
Daniel Gallegos Alia <dani@theyellowfactory.com>
2022-10-24 16:09:17 +0200

    add onhover to widgets.dart

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -14,3 +14,4 @@ export 'package:naples/src/widgets/async_action_icon_button.dart';
 export 'package:naples/src/widgets/back_forward_animation_widget.dart';
 export 'package:naples/src/widgets/tab_viewer.dart';
 export 'package:naples/src/widgets/card_top_bar.dart';
+export 'package:naples/src/widgets/onhover.dart';
