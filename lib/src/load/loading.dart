f600b4545663c1f633cee456dd36d9d679d0fd96
Daniel Gallegos Alia <dani@theyellowfactory.com>
2025-05-11 21:02:31 +0200

    Changes to loading

--- lib/src/load/loading.dart
+++ lib/src/load/loading.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'shimmer.dart';
 
 class LoadingNotification extends Notification {
   final bool loading;
@@ -9,10 +10,7 @@ class LoadingNotification extends Notification {
 
 class LoadingNotificationBuilder extends StatefulWidget {
   final Widget Function(BuildContext context, bool loading) builder;
-  const LoadingNotificationBuilder({
-    super.key,
-    required this.builder,
-  });
+  const LoadingNotificationBuilder({super.key, required this.builder});
 
   @override
   State<LoadingNotificationBuilder> createState() => _LoadingNotificationBuilderState();
@@ -39,60 +37,92 @@ class _LoadingNotificationBuilderState extends State<LoadingNotificationBuilder>
 }
 
 //Use this to absorb pointer events when loading
-class Loading extends StatelessWidget {
+class AbsorbPointerWhileLoading extends StatelessWidget {
   final Widget child;
-  const Loading({
-    super.key,
-    required this.child,
-  });
+  const AbsorbPointerWhileLoading({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return LoadingNotificationBuilder(
       builder: (context, loading) {
-        return AbsorbPointer(
-          absorbing: loading,
-          child: child,
-        );
+        return AbsorbPointer(absorbing: loading, child: child);
       },
     );
   }
 }
 
-enum LoadingProgressType {
-  linear,
-  circular,
-}
+enum LoadingProgressType { linear, circular, shadow }
 
 //Use this to show a progress indicator when loading and absorb pointer events
 class LoadingProgress extends StatelessWidget {
   final Widget child;
   final LoadingProgressType type;
-  const LoadingProgress({
+  const LoadingProgress({super.key, required this.child, this.type = LoadingProgressType.linear});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoadingNotificationBuilder(
+      builder: (context, loading) {
+        return LoadingProgressIndicator(loading: loading, type: type, child: child);
+      },
+    );
+  }
+}
+
+class LoadingProgressIndicator extends StatelessWidget {
+  final Widget child;
+  final LoadingProgressType type;
+  final bool loading;
+  const LoadingProgressIndicator({
     super.key,
+    required this.loading,
     required this.child,
     this.type = LoadingProgressType.linear,
   });
 
   @override
   Widget build(BuildContext context) {
-    return LoadingNotificationBuilder(
-      builder: (context, loading) {
-        return Stack(
-          children: [
-            child,
-            if (loading)
-              AbsorbPointer(
-                absorbing: true,
-                child: Container(
-                    color: Colors.white.withOpacity(0.5),
-                    child: type == LoadingProgressType.circular
-                        ? const Center(child: CircularProgressIndicator())
-                        : const LinearProgressIndicator()),
-              ),
-          ],
-        );
-      },
+    return Stack(
+      children: [
+        child,
+        if (loading)
+          AbsorbPointer(
+            absorbing: true,
+            child: Container(
+              color: Colors.black.withAlpha(50),
+              child:
+                  type == LoadingProgressType.circular
+                      ? const Center(child: CircularProgressIndicator())
+                      : type == LoadingProgressType.linear
+                      ? LinearProgressIndicator()
+                      : BlinkingShadowIndicator(height: 24.0, borderRadius: 12.0),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class BlinkingShadowIndicator extends StatelessWidget {
+  final double height;
+  final double borderRadius;
+  const BlinkingShadowIndicator({this.height = 100.0, this.borderRadius = 12.0, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return Container(color: const Color.fromARGB(196, 224, 224, 224));
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: Container(
+        width: double.infinity,
+        height: height,
+
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
     );
   }
 }
